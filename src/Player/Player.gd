370007369663f7f9c4e671a2caf4ea1b89f7f239
CaelRowley <caelrowley@gmail.com>
2020-04-24 18:12:18 +0200

    feat: add player movement animations

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -6,6 +6,8 @@ const MAX_SPEED = 100
 const ACCELERATION = 500
 const FRICTION = 500
 
+onready var animationPlayer = $AnimationPlayer
+
 # _function <== means callback function
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
@@ -13,8 +15,16 @@ func _physics_process(delta):
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if inputVector != Vector2.ZERO:
+		if(inputVector.x > 0):
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		if(inputVector.x > 0):
+			animationPlayer.play("IdleRight")
+		else:
+			animationPlayer.play("IdleLeft")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
