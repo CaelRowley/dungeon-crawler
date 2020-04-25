514c428c67926df762b6b7052ba30016119b841c
CaelRowley <caelrowley@gmail.com>
2020-04-25 20:40:03 +0200

    feat: add animation tree for player movement

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -6,7 +6,9 @@ const MAX_SPEED = 100
 const ACCELERATION = 500
 const FRICTION = 500
 
-onready var animationPlayer = $AnimationPlayer
+# onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 # _function <== means callback function
 func _physics_process(delta):
@@ -15,16 +17,12 @@ func _physics_process(delta):
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if inputVector != Vector2.ZERO:
-		if(inputVector.x > 0):
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", inputVector)
+		animationTree.set("parameters/Run/blend_position", inputVector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		if(inputVector.x > 0):
-			animationPlayer.play("IdleRight")
-		else:
-			animationPlayer.play("IdleLeft")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
