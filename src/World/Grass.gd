7b6eba7cae5fa0050e180528382e3a7a894b01f3
CaelRowley <caelrowley@gmail.com>
2020-04-30 16:43:53 +0200

    add new hitboxes and hurtboxes for player and grass

--- src/World/Grass.gd
+++ src/World/Grass.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 var GrassAnimation = load("res://Effects/GrassAnimation.tscn")
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grassAnimation = GrassAnimation.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassAnimation)
-		grassAnimation.global_position = global_position
-		queue_free()
+func createGrassEffect():
+	var grassAnimation = GrassAnimation.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassAnimation)
+	grassAnimation.global_position = global_position
+	queue_free()
+
+
+func _on_Hurtbox_area_entered(area):
+	createGrassEffect()
